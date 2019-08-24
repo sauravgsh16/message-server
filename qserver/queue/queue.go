8334b7a1d8104bbcd5cd90b136bd7d770dbfe551
Ghosh Saurav <GhoshSaurav@JohnDeere.com>
2019-08-24 19:05:34 +0530

    Minor addition

--- qserver/queue/queue.go
+++ qserver/queue/queue.go
@@ -2,6 +2,7 @@ package queue
 
 import (
 	"errors"
+	"fmt"
 	"sync"
 
 	"github.com/sauravgsh16/secoc-third/qserver/consumer"
@@ -9,14 +10,16 @@ import (
 )
 
 type Queue struct {
-	Name        string
-	list        *List
-	Closed      bool
-	mux         sync.Mutex
-	consumers   []*consumer.Consumer
-	consumerMux sync.RWMutex
-	ConnId      int64
-	deleteChan  chan *Queue
+	Name               string
+	list               *List
+	Closed             bool
+	mux                sync.Mutex
+	consumers          []*consumer.Consumer
+	consumerMux        sync.RWMutex
+	ConnId             int64
+	deleteChan         chan *Queue
+	readyChan          chan bool
+	currentConsumerIdx int
 }
 
 func NewQueue(name string, connId int64, deleteChan chan *Queue) *Queue {
@@ -25,7 +28,24 @@ func NewQueue(name string, connId int64, deleteChan chan *Queue) *Queue {
 		list:       newlist(),
 		consumers:  make([]*consumer.Consumer, 0),
 		deleteChan: deleteChan,
+		readyChan:  make(chan bool),
 	}
+}
+
+func (q *Queue) Start() {
+	go func() {
+		select {
+		case q.readyChan <- true:
+		default:
+		}
+		for _ = range q.readyChan {
+			if q.Closed {
+				fmt.Printf("Queue Closed: %s\n", q.Name)
+				break
+			}
+			q.processSingleEntry()
+		}
+	}()
 }
 
 func (q *Queue) Len() uint32 {
@@ -35,14 +55,6 @@ func (q *Queue) Len() uint32 {
 	}
 	return uint32(l)
 }
-
-func (q *Queue) ConsumerCount() uint32 {
-	return uint32(len(q.consumers))
-}
-
-// ************************
-//       IMPLEMENT
-//
 
 func (q *Queue) Close() {
 	q.mux.Lock()
@@ -58,8 +70,11 @@ func (q *Queue) Add(qm *proto.QueueMessage) bool {
 		return false
 	}
 	q.list.Append(qm)
-
-	// IMCOMPLETE
+	select {
+	case q.readyChan <- true:
+	default:
+	}
+	return true
 }
 
 func (q *Queue) Delete(ifUnused bool, ifEmpty bool) (uint32, error) {
@@ -86,6 +101,38 @@ func (q *Queue) Delete(ifUnused bool, ifEmpty bool) (uint32, error) {
 	return q.purgeQueueData(), nil
 }
 
+func (q *Queue) processSingleEntry() {
+	q.consumerMux.Lock()
+	defer q.consumerMux.Unlock()
+
+	length := len(q.consumers)
+	if length == 0 {
+		return
+	}
+	for count := 0; count < length; count++ {
+		q.currentConsumerIdx = (q.currentConsumerIdx + 1) % length
+		c := q.consumers[q.currentConsumerIdx]
+		c.Ping()
+	}
+}
+
+// ** Consumer related receivers **
+
+func (q *Queue) ConsumerCount() uint32 {
+	return uint32(len(q.consumers))
+}
+
+func (q *Queue) AddConsumer(c *consumer.Consumer) (uint16, error) {
+	if q.Closed {
+		return 0, nil // Check: if the error should be nil here ?
+	}
+
+	q.consumerMux.Lock()
+	q.consumers = append(q.consumers, c)
+	q.consumerMux.Unlock()
+	return 0, nil // Check: if it really needs to return the number of consumers ?
+}
+
 func (q *Queue) sendCancelConsumers() {
 	q.consumerMux.Lock()
 	defer q.consumerMux.Unlock()
@@ -100,6 +147,21 @@ func (q *Queue) sendCancelConsumers() {
 	q.consumers = make([]*consumer.Consumer, 0, 1)
 }
 
+func (q *Queue) removeConsumers(consumerTag string) {
+	q.consumerMux.Lock()
+	defer q.consumerMux.Unlock()
+
+	// Remove consumers based on consumerTag
+	for i, c := range q.consumers {
+		if c.ConsumerTag == consumerTag {
+			q.consumers = append(q.consumers[:i], q.consumers[i+1:]...)
+		}
+	}
+	if len(q.consumers) == 0 {
+		// INCOMPLETE
+	}
+}
+
 func (q *Queue) purgeQueueData() uint32 {
 	length := q.list.Len()
 	q.list.removeRef()
@@ -108,8 +170,4 @@ func (q *Queue) purgeQueueData() uint32 {
 
 func (q *Queue) GetOne(mrh ...proto.MessageResourceHolder) (*proto.QueueMessage, *proto.Message) {
 	return nil, nil
-}
-
-func (q *Queue) AddConsumer(c *consumer.Consumer) (uint16, error) {
-	return 0, nil
 }
